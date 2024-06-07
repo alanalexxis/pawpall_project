f2789c9be50c4df4e6de3100525de9bf49b54310
alanalexxis <alanalexxis3@gmail.com>
2024-06-07 11:54:31 -0600

    se eliminan botones de shader al 3d effect

--- components/model.tsx
+++ components/model.tsx
@@ -1,22 +1,20 @@
 import React, { useRef } from "react";
-import { useControls } from "leva";
 import { fragment, vertex } from "./shader";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useTexture, useAspect } from "@react-three/drei";
 import * as THREE from "three";
 import { transform } from "framer-motion";
 
-export default function Model({ scrollProgress }) {
+export default function Model({ scrollProgress }: { scrollProgress: any }) {
   const image = useRef<THREE.Mesh | null>(null);
   const texture = useTexture("/images/car.jpg");
   const { width, height } = texture.image;
   const { viewport } = useThree();
   const scale = useAspect(width, height, 0.3);
 
-  const { amplitude, waveLength } = useControls({
-    amplitude: { value: 0.25, min: 0, max: 2, step: 0.1 },
-    waveLength: { value: 5, min: 0, max: 20, step: 0.5 },
-  });
+  // Define los valores de amplitude y waveLength directamente
+  const amplitude = 0.25;
+  const waveLength = 5;
 
   const uniforms = useRef({
     uTime: { value: 0 },
@@ -44,7 +42,6 @@ export default function Model({ scrollProgress }) {
       );
       image.current.scale.x = scaleX;
       image.current.scale.y = scaleY;
-
       // Adjust texture to new scale
       const scaleRatio = scaleX / scaleY;
       const aspectRatio = width / height;
@@ -53,14 +50,12 @@ export default function Model({ scrollProgress }) {
         1,
         aspectRatio / scaleRatio
       );
-
       // animate wave based on progress of the scroll
       const modifiedAmplitude = transform(
         scrollProgress.get(),
         [0, 1],
         [amplitude, 0]
       );
-
       image.current.material.uniforms.uTime.value += 0.04;
       image.current.material.uniforms.uAmplitude.value = modifiedAmplitude;
       image.current.material.uniforms.uWaveLength.value = waveLength;
