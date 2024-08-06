27c234f814575a9903d371534ad5af26f7fdcc71
alanalexxis <alanalexxis3@gmail.com>
2024-08-05 19:05:28 -0600

    se agrega hero de buscador razas

---
+++ components/search/hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+import { SearchBar } from "./search";
+
+function Hero() {
+  return (
+    <div className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-yellow-200 via-emerald-200 to-yellow-200 text-white flex items-center justify-center h-auto py-10">
+      <div className="absolute top-4 left-4 right-4 ">
+        <SearchBar />
+      </div>
+      <div className="container mx-auto px-4 max-w-4xl flex flex-col md:flex-row items-center justify-between pt-6">
+        <div className="flex-1 text-center md:text-left mb-6 md:mb-0">
+          <h1 className="text-4xl font-bold mb-2">Golden Retriever</h1>
+          <p className="text-xl mb-4">inteligente / amigable / devoto</p>
+          <Badge className="bg-yellow-300 text-black">GRUPO DEPORTIVO</Badge>
+        </div>
+        <div className="flex-1 relative mt-6 md:mt-0">
+          <img
+            src="/labrador.jpg"
+            alt="Golden Retriever"
+            className="w-full h-auto rounded-lg shadow-lg transition-transform transform hover:scale-105"
+          />
+          <button className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white text-black rounded-full p-3 shadow-md transition-transform hover:scale-110">
+            &lsaquo;
+          </button>
+          <button className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white text-black rounded-full p-3 shadow-md transition-transform hover:scale-110">
+            &rsaquo;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
